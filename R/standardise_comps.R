fe4f2f13c7496920e2377ece431608f99e6c9b7b
Ty Stanford <tystan@gmail.com>
2022-12-22 18:50:06 +0000

    version 0.1.0

---
+++ R/standardise_comps.R
@@ -0,0 +1,22 @@
+
+
+
+standardise_comps <- function(dataf, comps) {
+
+  # standardise comps
+  comp_totals <- rowSums(dataf[, comps])
+  comps_to_use <- compositions::acomp(dataf[, comps])
+
+  message("---\nThese are the quartiles of the summed compositions:\n")
+  message(paste(kable(quantile(comp_totals, seq(0, 1, by = 0.25))), collapse = "\n"))
+  message("---\n")
+  dataf[, comps] <- as.data.frame(comps_to_use)
+
+  ## equally could use: dataf[, comps] / matrix(comp_totals, ncol = n_comp, nrow = n)
+  # i.e.,note that these are equal
+  # as.data.frame(compositions::acomp(dataf[,comps])) -
+  # dataf[, comps] / matrix(comp_totals, ncol = n_comp, nrow = n)
+
+  return(dataf)
+
+}
